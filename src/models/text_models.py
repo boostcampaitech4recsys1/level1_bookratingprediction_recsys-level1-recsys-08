845ae165f6d286e2fb377e09814cd2ae8f659394
netsus <cwjcsk@naver.com>
2022-11-01 19:25:53 +0900

    [text_model.py] tk0 postfix

--- src/models/text_models.py
+++ src/models/text_models.py
@@ -132,7 +132,7 @@ class DeepCoNN:
                 loss_list.append([epoch, total_loss/n, val_total_loss/val_n, 'Model saved'])
             else:
                 loss_list.append([epoch, total_loss/n, val_total_loss/val_n, 'None'])
-            tk0.set_postfix(train_loss=total_loss/n, valid_loss=val_total_loss/val_n)
+            tk0.set_postfix(t_ls=total_loss/n,v_ls=val_total_loss/val_n,m_ls=minimum_loss)
         return minimum_loss
 
 
