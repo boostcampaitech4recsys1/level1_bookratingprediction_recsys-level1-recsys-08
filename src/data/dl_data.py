170c50d3f2dcdceaa687098734e3d809067d379c
nahyunLee <awsed2486@gmail.com>
2022-11-03 02:47:05 +0900

    🥲

--- src/data/dl_data.py
+++ src/data/dl_data.py
@@ -4,15 +4,68 @@ from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader, Dataset
+from .utils import make_category_high, preprocessing_book_author, \
+                    edit_once_rated_book, edit_once_rated_user, publisher_modify, \
+                    location_modify_country, location_modify_state
+from .context_data import age_map
+
+
+# train에 feature merge해주는 함수
+def train_merge_feature(users, books, train, user_feat_list, book_feat_list):
+    # 피쳐 전처리
+    if 'book_author' in set(book_feat_list):
+        books = preprocessing_book_author(books)
+    if 'publisher' in set(book_feat_list):
+        books = publisher_modify(books)
+    # if 'location' in set(book_feat_list):
+    #     if len(set(users.columns).intersection(location_set))==3: # 기존 users에 city, state, country가 존재한다면,
+    #         pass
+    #     else:
+    #         users['location_city'] = users['location'].apply(lambda x: x.split(',')[0].strip())
+    #         users['location_state'] = users['location'].apply(lambda x: x.split(',')[1].strip())
+    #         users['location_country'] = users['location'].apply(lambda x: x.split(',')[2].strip())
+    #         # 🍁🍁🍁 location 전처리, 주의❗️ 아래의 두 함수를 호출하면 데이터 로드가 약 1분 30초가 소요됨.
+    #         users = location_modify_country(users)
+    #         users = location_modify_state(users)
+    #         users['location_city'] = users['location_city'].map(loc_city2idx)
+    #         users['location_state'] = useres['location_state'].map(loc_state2idx)
+    #         users['location_country'] = users['location_country'].map(loc_country2idx)
+    
+    # 피쳐 인덱싱
+    users['age'] = users['age'].fillna(int(users['age'].mean()))
+    users['age'] = users['age'].apply(age_map)
+    category2idx = {v:k for k,v in enumerate(books['category'].unique())}
+    categoryhigh2idx = {v:k for k,v in enumerate(books['category_high'].unique())}
+    publisher2idx = {v:k for k,v in enumerate(books['publisher'].unique())}
+    language2idx = {v:k for k,v in enumerate(books['language'].unique())}
+    author2idx = {v:k for k,v in enumerate(books['book_author'].unique())}
+
+    books['category'] = books['category'].map(category2idx)
+    books['category_high'] = books['category_high'].map(categoryhigh2idx)
+    books['publisher'] = books['publisher'].map(publisher2idx)
+    books['language'] = books['language'].map(language2idx)
+    books['book_author'] = books['book_author'].map(author2idx)
+
+    if user_feat_list:
+        train = train.merge(users[['user_id']+user_feat_list], how='left', on='user_id')
+    if book_feat_list:
+        train = train.merge(books[['isbn']+book_feat_list], how='left', on='isbn')
+    return train
+        
 
 def dl_data_load(args):
-
     ######################## DATA LOAD
     users = pd.read_csv(args.DATA_PATH + 'users.csv')
     books = pd.read_csv(args.DATA_PATH + 'books.csv')
     train = pd.read_csv(args.DATA_PATH + 'train_ratings.csv')
     test = pd.read_csv(args.DATA_PATH + 'test_ratings.csv')
     sub = pd.read_csv(args.DATA_PATH + 'sample_submission.csv')
+
+    # 🍁🍁🍁 books에 category_high 추가
+    books = make_category_high(books)
+
+    # train에 feature merge해주는 함수
+    train = train_merge_feature(users, books, train, user_feat_list=['age'], book_feat_list=[])
 
     ids = pd.concat([train['user_id'], sub['user_id']]).unique()
     isbns = pd.concat([train['isbn'], sub['isbn']]).unique()
@@ -31,7 +84,7 @@ def dl_data_load(args):
     sub['isbn'] = sub['isbn'].map(isbn2idx)
     test['isbn'] = test['isbn'].map(isbn2idx)
 
-    field_dims = np.array([len(user2idx), len(isbn2idx)], dtype=np.uint32)
+    field_dims = np.array([len(user2idx), len(isbn2idx), 7], dtype=np.uint32)
 
     data = {
             'train':train,
@@ -45,7 +98,6 @@ def dl_data_load(args):
             'user2idx':user2idx,
             'isbn2idx':isbn2idx,
             }
-
 
     return data
 
